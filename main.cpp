707f9a506d2edc71dd57d68a1eb9b112beff219a
PWSZ Nowy Sącz <cpp.programowanie@gmail.com>
2019-11-18 16:54:39 +0100

    trzecia zmiana

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tstring.h"
+#include "operatory.h"
 using namespace std;
 
 auto fun(const char* s) {
@@ -11,11 +12,8 @@ int main() {
     TString s2("dzien dobry");
     TString s3 = move(s2);
     s1 = fun("drugi obiekt");   // move(s3);
-    for ( auto& c : s1 ) cout << (c=toupper(c)) << "-";
-    cout << endl;
-    for ( auto c : s1 ) cout << c << "=";
-    cout << endl;
-    for ( size_t i = 0; i < s1.size(); ++i ) cout << s1[i] << "#";
-    cout << endl;
+    cout << s1 << endl;
+    cin >> s1; 
+    cout << s1 << endl;
     cout << "----------------" << endl;
 }
